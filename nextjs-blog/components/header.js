91eba41c24b82f9b08fa96f771fd233354492ac1
David <dvloedman@protonmail.com>
2020-12-15 09:45:43 -0800

    Header for blog. Create layout.

---
+++ nextjs-blog/components/header.js
@@ -0,0 +1,5 @@
+export default function Header(){
+    return <header>
+        <h1>David's Test Blog</h1>
+    </header>
+}
--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,13 +1,26 @@
 import styled from 'styled-components'
+import Head from 'next/head'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 export default function Layout({children}){
 
     const Container = styled.section`
         font-size: 1.5rem;
-        text-align: center;
-        background: lightgrey;
-        color: darkred;
+        background: #F9F2CE;
+        color: #333;
+        padding: 0 1rem;
+        margin: 2rem auto;
+        max-width: 40rem;
     `;
 
-    return <Container>{children}</Container>
+    return <Container>
+        <Head>
+        <title>David does Nextjs</title>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header/>
+        {children}
+        
+        </Container>
 }
