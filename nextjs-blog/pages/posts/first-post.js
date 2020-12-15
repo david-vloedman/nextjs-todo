a7146676c906ab24ec1c6ba3574b4fb56b23e579
David <dvloedman@protonmail.com>
2020-12-14 17:05:38 -0800

    First post, basics of super easy routing via next/Link

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function FirstPost(){
+    return <div>
+        <Head>
+            <title>David's First Page</title>
+        </Head>
+        <h1>FIRST POST GOALS ACCHOMPOLISHED</h1>
+    </div>
+    
+}
