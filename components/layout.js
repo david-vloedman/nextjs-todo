af65f3d0be844ec3c3948a88698708532d89a325
David <dvloedman@protonmail.com>
2021-01-14 14:28:40 -0800

    Add material-ui SVG Icons package

--- components/layout.js
+++ components/layout.js
@@ -10,6 +10,8 @@ export default function Layout({children}){
             <Head>
                 <title>David does Nextjs</title>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
             </Head>
             <Header/>
             <Nav/>
