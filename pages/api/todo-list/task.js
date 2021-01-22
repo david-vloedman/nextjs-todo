9150fc217b8982079a080f844c6b220e17792449
David <dvloedman@protonmail.com>
2021-01-22 09:06:07 -0800

    commit before PR

--- pages/api/todo-list/task.js
+++ pages/api/todo-list/task.js
@@ -7,14 +7,13 @@ handler.use(middleware);
 
 handler.get(async (req, res) => {
     // const doc = await req.db.collection("task").findOne();
-    const doc = await req.db.collection("task").findOne(req.taskId || null);
-    console.log(doc);
+    const doc = await req.db.collection("tasks").findOne();
     res.status(200).end(JSON.stringify(doc));
-    
 });
 
 handler.post(async (req, res) => {
-
+    const task = req.task;
+    req.db.collection("tasks").insertOne(task);
 });
 
 handler.put(async (req, res) => {
